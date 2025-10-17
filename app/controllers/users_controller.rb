2362bf4c538d6c40c30b39959b443bb88227b3c6
dgocrz <diego.cruz02.bp@gmail.com>
2025-10-17 10:40:52 -0600

    Creamos el formulario de registro junto con los modelos, controladores, rutas y base de datos

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def index
+
+
+  end
+
+  def new
+
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+
+   
+    @user =User.new(user_params)
+
+    #Comprobamos la creación del usuario 
+
+    if @user.save
+      redirect_to @user, notice: "Usuario creado con exito"
+    else
+      render:new,status: :unprocessable_entity 
+    end
+  end
+
+  def user_params
+    #Se solicitan los  parametros del usuario como nombre, email y contraseña 
+
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "users/index"
+  get "users/new"
+  get "users/show"
+  get "users/create"
+  get "users/search"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,4 +16,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+Rails.application.routes.draw do
+  resources :users,only: [:new,:create,:show]
+  root "users#new"
+end
+
 end
